422b299abf725e40d75ab0a3e5183331838e9b2b
Cláudio Leal <claudiolealti@gmail.com>
2020-05-21 11:15:30 -0300

    Versão 7 - Criação da classe ClothingItem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,71 +1,32 @@
 package com.company;
 
-import com.company.utilities.CalcHelper;
+import com.company.model.ClothingItem;
 
-// Sintaxe para importar todas as classes da pasta java > util.
-// Não é a melhor prática,...os programadores preferem "documentar"...
-// as classes que estão importando.
-import java.util.*;
-//import java.util.InputMismatchException;
-//import java.util.Scanner;
+import java.text.NumberFormat;
 
-// Desafio da calculadora "super complexa", que impede que o usuário...
-// entre com valores diferente dos esperados (exemplos, textos).
 public class Main {
+    public static void main (String [] args) {
+        // Aqui estou instanciando a classe ClothingItem, com a declaração...
+        // da variável item.
+        // Observe que chamando os métodos sets.
+        var item = new ClothingItem();
+        item.setType("Shirt");
+        item.setSize("M");
+        item.setQuantity(3);
+        item.setPrice(19.45);
 
-    public static void main(String[] args) {
-	    // variável sc recebendo a criação da instância da classe newScanner.
-        // lembrando que a biblioteca é importada automaticamente no início do código.
-        var sc = new Scanner(System.in);
-
-        // variáveis d1 e d2,...responsáveis pela entrada do usuário.
-        double d1, d2;
-        // try-catch aplicado abaixo. É ele que fará o teste sobre os valores...
-        // inseridogs pelo usuário.
-        // lembrando que é o método nextDouble é o responsável por impedir...
-        // a entrada de valores diferentes de double.
-        try {
-            System.out.println("Valor 1:");
-            d1 = sc.nextDouble();
-            sc.nextLine();
-
-            System.out.println("Valor 2:");
-            d2 = sc.nextDouble();
-            sc.nextLine();
-        } catch (InputMismatchException e) {
-            System.out.println("Formato inválido");
-            // lembrando que este return é apenas para que returne "null ou zero",...
-            // para o método. O código será abortado nesta fase.
-            return;
-        }
-        // Solicitação para que o usuário digite a operação:
-        System.out.println("Operação: (+ - / *): ");
-        var operacao = sc.nextLine();
-        double result;
-
-        switch (operacao){
-            case "+":
-                result = CalcHelper.adittion(d1,d2);
-                break;
-
-            case "-":
-                result = CalcHelper.subtraction(d1,d2);
-                break;
-
-            case "*":
-                result = CalcHelper.multiplication(d1,d2);
-                break;
-
-            case "/":
-                result = CalcHelper.division(d1,d2);
-                break;
-
-            default:
-                System.out.println("Operador inválido!");
-                return;
-        }
-        System.out.println(result);
+        // totalizando:
+        // Aqui, já estou chamando os métodos gets.
+        // Formatando os valores, utilizando o método adequado para a aplicação da...
+        // moeda corrente.
+        // variável output ficará responsável pela impressão na tela, com os valores...
+        // devidamente formatados.
+        // Ao final, impressão da mensagem formatada.
+        var total = item.getPrice() * item.getQuantity();
+        var formatNumber = NumberFormat.getCurrencyInstance();
+        var output = String.format("Your %s order will cust %s",
+                item.getType(),
+                formatNumber.format(total));
+        System.out.println(output);
     }
 }
-
-
---
+++ src/com/company/model/ClothingItem.java
@@ -0,0 +1,58 @@
+// Observe aqui a referência ao local onde o pacote se encontra.
+// Relembrando,..quando esta referência existe, a classe não é a default.
+// Neste classe, vamos ter 4 métodos que farão referência ao tipo, tamanho, preço e quantidade.
+package com.company.model;
+
+// public: poderá ser chamada de qualquer local da aplicação.
+// class: representa a declaração de uma classe, ou seja, isto é uma classe.
+// ClothingItem: nome da classe.
+public class ClothingItem {
+    // todas estas variáveis (que serão os campos) só poderão ser chamadas...
+    // a partir desta classe, pois estão declaradas como "private".
+    // Generate > Getter And Setter > Selecionar tudo.
+    // Com isso, métodos GETs e SETs foram criados com base nas variáveis declaradas.
+    // que permaneceram no código.
+
+    private String type;
+    private String Size;
+    private int quantity;
+    private Double price;
+
+    // repare que os métodos foram declarados como públicos, o que permitirá que...
+    // sejam chamados de qualquer parte da aplicação.
+    // public: poderá ser chamado de qualquer ponto da aplicação.
+    // String ou void: tipo de retorno esperado pelo método.
+    // no caso dos métodos sets: argumento do método que passa como parâmetro, o valor...
+    // da variável type.
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return Size;
+    }
+
+    public void setSize(String size) {
+        Size = size;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
