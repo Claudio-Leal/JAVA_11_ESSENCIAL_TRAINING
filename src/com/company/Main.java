971e538134e2902e9d8066e3f8ac917a0a789d96
Cláudio Leal <claudiolealti@gmail.com>
2020-05-21 18:13:52 -0300

    Versão 17 - Herança - Início - Use objects as their super types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,5 @@
 package com.company;
 
-// Todas as classes que estão sendo utilizadas aqui, inclusive a última: Shirt.
-// Através da função CODE > OPTIMAZE IMPORTS, a importação da classe ClothingItem será excluída..
-// deste código, pois ela não está sendo utilizada no momento.
-// o que ocorre agora é a chamada da classe Shirt,...SUBCLASSE da SUPERCLASSE ClothingItem.
-// E esta (Shirt) referencia (ClothingItem).
-// Avaliando o código, vemos que realmente o método ClothingItem foi excluído,...agora estamos...
-// chamando apenas Shirt,...então não é mais necessário importar ClothingItem.
 
 import com.company.model.ClothingSize;
 import com.company.model.Shirt;
@@ -15,12 +8,6 @@ import java.text.NumberFormat;
 
 public class Main {
     public static void main (String [] args) {
-
-
-        // Criação de uma instância da classe Shirt,...
-        // em que os valores das variáveis size, quantity e price estão sendo passados.
-        // passando apenas valores para três parâmetros, pois "type" foi excluído dos argumentos...
-        // do CONSTRUCTOR METHOD Shirt.
 
         var item = new Shirt (
                 ClothingSize.G,
--- src/com/company/model/Shirt.java
+++ src/com/company/model/Shirt.java
@@ -1,53 +1,7 @@
 package com.company.model;
 
-// Utiliza-se a palavra chave extends para "relacionar" a classe com outra classe.
-// Neste caso, estamos extendendo a classe Shirt.java à classe ClothingItem.
-// Agora, esta classe está herdando todas as funcionalidades da classe ClothingItem.
-// Com isso, posso acessar todos os CONSTRUCTOR METHODS da classe ClothingItem.
-// Com isso, posso acessar todos os METHODS da classe ClothingItem.
-// Mas o que está marcado como privado, permanece privado.
-// Serão os os métodos gets e o constructor ClothingItem que fará acesso aos objetos privados (variáveis...
-// que identificam o ITEM ROUPA.
-// Lembrnado que esta classe (Shirt.java) é uma subclasse da classe ClothingItem,...e isso ocorreu...
-// quando utilizei o extends.
-// Ao incluir o "extends", não houve nenhuma marcação especial em ClothingItem,...mas concluo...
-// que ela é a SUPERCLASSE desta SUBCLASSE.
 
 public class Shirt extends ClothingItem {
-
-    // Com a utilização de CODE > GENERATE > CONSTRUCTOR, inseri este CONSTRUCTOR METHOD.
-    // Note que:
-    // public: poderá ser acessada a partir de qualquer parte da aplicação.
-    // Shirt: nome do CONSTRUCTOR METHOD.
-    // parâmetros: as quatro variáveis declaradas na super classe.
-    // não tem retorno, pois se trata de um CONSTRUCTOR METHOD.
-    // super: é uma palavra chave, na verdade, uma variável. Definições abaixo...
-    // no caso, ela está referenciando as quatro variáveis (os quatro objetos) declaradas em ClothingItem.
-    // Mais uma vez, digo que estes quatro objetos (estas quatro variáveis), são privadas em ClothingItem.
-    // super, pelo que entendi, possui a função de dizer: sim,...estes são objetos existentes,..declarados,...
-    // na superclasse. Por isso, os referencia,...recebendo os valores dos mesmos pelos parâmetros.
-
-    // super: The super keyword in java is a reference variable that is used to refer parent class...
-    // objects.  The keyword “super” came into the picture with the concept of Inheritance.
-    //    A super palavra-chave em java é uma variável de referência usada para referenciar objetos...
-    //    de classe pai. A palavra-chave “super” entrou em cena com o conceito de herança.
-    //    É usado principalmente nos seguintes contextos:
-    //            1. Uso de super com variáveis: este cenário ocorre quando uma classe derivada e...
-    //            uma classe base têm os mesmos membros de dados.
-    //            Nesse caso, há uma possibilidade de ambiguidade para a JVM.
-
-
-    // Este é o CONSTRUCTOR METHOD original:
-//    public Shirt(String type, ClothingSize size, int quantity, Double price) {
-//        super(type, size, quantity, price);
-//    }
-
-    // Uma pequena alteração precisou ser feita, já que aqui estamos tratando apenas de Shirt:
-    // em super, incluí ClothingItem.SHIRT (constante declarada na classe ClothingItem).
-    // Nos argumentos, excluí o parâmetro String type,...pois não faz mais sentido,...já que estamos...
-    // criando uma classe especificamente para definir SHIRT. Neste caso, apenas três parâmetros estão...
-    // sendo definidos nos argumentos deste CONSTRUCTOR METHOD.
-    // Por fim, esta classe passará a ser instanciada em Main.java.
     public Shirt(ClothingSize size, int quantity, Double price) {
         super(ClothingItem.SHIRT, size, quantity, price);
     }
