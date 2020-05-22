b203f0c7de31d802a46594ed972eb5bdf060362a
Cláudio Leal <claudiolealti@gmail.com>
2020-05-22 11:46:06 -0300

    Versão 23 - Manage key-values pairs with maps - 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,16 @@
-/* Manage key-values pairs with maps
+/*
+Manage key-values pairs with maps
 Gerenciar pares de valores-chave com o Maps
+Para criar uma lista desordenada.
+Cada item da lista, possui um valor chave como par.
+
+Tivemos anteriormente:
+Array
+Lists
+e agora: Maps
+Todos são estruturas de dados.
+Maps também é uma interface,...
+
 */
 
 
@@ -11,10 +22,24 @@ import com.company.model.Hat;
 import com.company.model.Shirt;
 
 import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main (String [] args) {
 
+        // Abaixo, bloco com os Maps.
+        // A declaração é parecida com Lists, usando <>.
+        // Dentro dos <>, entrarão o par de valor chave + item.
+        // Esquerda: tipo de dado.
+        // Chave: String.
+        // Tipo de item: ClothingItem.
+        // objetos: nome do Map.
+        // new HashMap: criação de uma instância da classe HashMap. Definição no github (pullrequest).
+        // não é necessário passar os parâmetros dentro dos <> do HashMap, pois isso já foi feito...
+        // no Map.
+
+        Map<String , ClothingItem> objetos = new HashMap<>();
 
         var shirt = new Shirt (
                                 ClothingSize.G,
