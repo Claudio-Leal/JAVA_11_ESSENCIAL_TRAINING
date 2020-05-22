84e536c4f117a220fccf4e3fafa863bb2fdf0cd7
Cláudio Leal <claudiolealti@gmail.com>
2020-05-22 09:22:07 -0300

    Versão 21 - Store values in simple array - final

--- src/com/company/model/Hat.java
+++ src/com/company/model/Hat.java
@@ -1,8 +1,5 @@
 package com.company.model;
 
-// o nome do CONSTRUCTOR METHOD foi alterado porque o "nome" combina com...
-// o nome da própria classe. É sempre assim,...já registrei isso antes.
-
 public class Hat extends ClothingItem {
     public Hat(ClothingSize size, int quantity, Double price) {
         super(ClothingItem.HAT, size, quantity, price);
